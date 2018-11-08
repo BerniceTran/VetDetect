b082ed7c056530de2c829d0be3fa25a360a7c09c
Emmanuil Simkhayev <e.simkhayev@gmail.com>
2018-11-07 19:57:36 -0500

    test model and controller to post

--- vetdetect_backend/controllers/index.js
+++ vetdetect_backend/controllers/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
+router.use('/alt', require('./alt'));
+router.use('/posts', require('./posts'));
 
 
 module.exports = router;
