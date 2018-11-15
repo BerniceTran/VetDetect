dd9b48d0145b180fa4f569276bdf824043117e1d
Bernice Tran <bernicetran94@gmail.com>
2018-11-14 19:31:03 -0500

    Adding vet model

--- models/Clinic.js
+++ models/Clinic.js
@@ -1,12 +1,76 @@
 module.exports = (sequelize, DataTypes) => {
-  var Posts = sequelize.define('', {
-    post: DataTypes.TEXT,
-    author: DataTypes.STRING
+  const Clinic = sequelize.define('Clinic', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+      primaryKey: true;
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isAlphanumeric: true,
+      },
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+	zip: {
+	  type: DataTypes.INTEGER,
+	  allowNull: false,
+	  unique: true,
+	  validate: {
+	    notEmpty: true,
+	    isEmail: true,
+	  },
+	},
+	latitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    longitude: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+
   });
 
-  Posts.associate = (models) => {
-    // associations can be defined here
-  }
-
-  return Posts;
+  return Clinic;
 };
