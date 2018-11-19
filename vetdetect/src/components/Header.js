56ea306f867fe06d5832d41da92996db86c2a32b
Emmanuil <e.simkhayev@gmail.com>
2018-11-18 19:15:05 -0500

    navbar with login and signup options

--- vetdetect/src/components/Header.js
+++ vetdetect/src/components/Header.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import pawPrint from './paw-print.png';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import Button from 'react-bootstrap/lib/Button';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import SplitButton from 'react-bootstrap/lib/SplitButton'
 
-import { Link } from 'react-router-dom';
 class Header extends React.Component {
     render() {
       return (
-        <nav className="navbar navbar-light">
           <div>
-            <div   className="navbar-brand">
-              <h1 style = {{color:'black'}}>
-            <img src= {pawPrint} height={'75px'} width= {'75px'}/>
-               {'VetDetect'} 
-              </h1> 
-            </div>
-          </div>
-        </nav>
+            <Navbar>
+            <Navbar.Header height = {'80px'}>
+                <Navbar.Brand >
+                    <img src={pawPrint}/>
+                </Navbar.Brand>
+                <Navbar.Brand>
+                    <a href="/" >VetDetect</a>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Navbar.Form pullRight>
+                    <FormGroup>
+                    <FormControl  type ="text" placeholder = "Email"/>
+                    {' '}
+                    <FormControl type ="text" placeholder = "Password"/> 
+                    </FormGroup>
+                    {' '}
+                    <Button color ={'blue'} type="submit">Login</Button>
+                    {' '}
+                    <SplitButton pullRight title = {'Sign up'}>
+                        <MenuItem eventKey="1">Patient</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem eventKey="2">Veterinarian</MenuItem>
+                    </SplitButton>
+            </Navbar.Form>
+        </Navbar>;
+        </div>
       );
     }
   }
