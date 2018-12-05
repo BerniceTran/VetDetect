5ba11fbff2d02dd68be6c7146b433a703f925b26
Emmanuil <e.simkhayev@gmail.com>
2018-12-05 01:41:57 -0500

    states and props for search bar

---
+++ vetdetect/src/components/SearchResults/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from '../Home';
+import Header from '../Header'
+class SearchResults extends React.Component {
+    render() {
+      return (
+        <div className='Results'>          
+            
+        </div>
+      );
+    }
+  }
+
+  export default(SearchResults);
